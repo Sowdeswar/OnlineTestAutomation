da505f33c1b754584f9782f857e396affb899e7d
Sowdeswar <72703610+Sowdeswar@users.noreply.github.com>
2024-06-26 10:13:40 -0700

    Update Hooks.java

--- OnlineTestGenesys/src/test/java/onlineTest/Genesys/StepDefinitions/Hooks.java
+++ OnlineTestGenesys/src/test/java/onlineTest/Genesys/StepDefinitions/Hooks.java
@@ -1,2 +1,17 @@
-package onlineTest.Genesys.StepDefinitions;public class Hooks {
+package onlineTest.Genesys.StepDefinitions;
+
+public class Hooks {
+  public  static Scenario scenario;
+
+    @After
+    public void afterScenario() {
+        if(scenario.isFailed()) {
+            System.out.println("Scenario Failed");
+        }
+    }
+
+    @Before
+    public void beforeScenario(Scenario scenario) {
+        Hooks.scenario = scenario;
+    }
 }
